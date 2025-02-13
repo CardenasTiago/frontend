14ea6f0aea6d961f35f47c55f8028909aef059a9
nacho <nachoagusss1@gmail.com>
2025-02-13 19:51:53 -0300

    set up boton start room

---
+++ src/components/test/StartRoom.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="flex justify-center mt-5">
+        <button class="btn btn-primary start-room">Iniciar</button>
+    </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue';
+
+
+const room = ref(null);
+const error = ref('');
+
+onMounted(() => {
+  const storedRoom = localStorage.getItem('currentRoom');
+  if (storedRoom) {
+    try {
+
+      room.value = JSON.parse(storedRoom);
+
+    } catch (e) {
+      error.value = 'Error al leer los datos de la sala.';
+      console.error(e);
+    }
+  } else {
+    error.value = 'No se encontraron datos de la sala en el almacenamiento local.';
+  }
+});
+
+const WS_URL = 'ws://localhost:3000/v1/rooms/' + room.id;
+
+
+
+</script>
