62b7570c33e352d572c632b530f22d56551bf137
Marcos <marcosrsoto90@gmail.com>
2025-03-24 01:19:29 +0000

    Se define alto de la tarjeta,se configura foto,arreglo de backbutton

--- src/components/reusable/BackButton2.vue
+++ src/components/reusable/BackButton2.vue
@@ -1,5 +1,5 @@
 <template>
-    <button @click="goBack" class="btn btn-circle bg-secondary/20 hover:bg-secondary/30 border-none hidden lg:flex md:flex items-center justify-center">
+    <button @click="goBack" class="btn btn-circle bg-secondary/20 hover:bg-secondary/30 border-none items-center justify-center">
         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
         </svg>
