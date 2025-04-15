141afa7e48dd4559890f0a0d149221f726ee807e
Ignacio Sanchez <nachoagusss1@gmail.com>
2025-04-15 19:48:32 -0300

    se agrego scrollbar en caso de que hayan muchos votos para una opcion

--- src/components/room/lobby/components/UserVotes.vue
+++ src/components/room/lobby/components/UserVotes.vue
@@ -1,43 +1,53 @@
 <template>
     <div class="user-votes p-4 rounded-lg shadow-md shadow-secondary flex-auto">
-        <h2 class="text-xl font-bold mb-3">Votos por propuesta</h2>
-        <div v-for="option in proposal.options" :key="option.option_Id" class="option mb-6">
-            <h4 class="text-base font-semibold text-primary "> {{ option.option_value }}</h4>
-            <!-- Si la opción tiene votos, se listan -->
-            <ul v-if="option.votes && option.votes.length > 0" class="ml-4 list-disc">
-                <li v-for="vote in option.votes" :key="vote.vote_id" class="flex items-center gap-2">
-                    <span class="font-medium">{{ vote.username }}</span>
-                </li>
-            </ul>
-            <!-- Mensaje si no hay votos en la opción -->
-            <p v-else class="text-sm text-gray-500 ml-4">Sin votos</p>
-        </div>
+      <h2 class="text-xl font-bold mb-3">Votos por propuesta</h2>
+      <div
+        v-for="option in proposal.options"
+        :key="option.option_Id"
+        class="option mb-6"
+      >
+        <h4 class="text-base font-semibold text-primary">
+          {{ option.option_value }}
+        </h4>
+        <!-- Si la opción tiene votos, se listan con scroll si exceden la altura máxima -->
+        <ul
+          v-if="option.votes && option.votes.length > 0"
+          class="ml-4 list-disc max-h-60 overflow-y-auto"
+        >
+          <li
+            v-for="vote in option.votes"
+            :key="vote.vote_id"
+            class="flex items-center gap-2"
+          >
+            <span class="font-medium">{{ vote.username }}</span>
+          </li>
+        </ul>
+        <!-- Mensaje si no hay votos en la opción -->
+        <p v-else class="text-sm text-gray-500 ml-4">Sin votos</p>
+      </div>
     </div>
-</template>
-
-<script setup>
-const props = defineProps({
+  </template>
+  
+  <script setup>
+  const props = defineProps({
     proposal: {
-        type: Object,
-        required: true
+      type: Object,
+      required: true
     }
-});
-</script>
-
-<style scoped>
-.user-votes {
+  });
+  </script>
+  
+  <style scoped>
+  .user-votes {
     @apply bg-base-100;
     max-width: 800px;
     margin: 0 auto;
-}
-
-.proposal {
-    border-radius: 15px;
-}
-
-.option {
+  }
+  
+  .option {
     @apply bg-neutral;
     padding: 0.5rem;
     border-radius: 15px;
-}
-</style>
+  }
+  </style>
+  
