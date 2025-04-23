48f88c8b559611f57bee3cee62b42865c39e7d9e
Ignacio Sanchez <nachoagusss1@gmail.com>
2025-04-23 16:28:40 -0300

    correciones de estilos menores

--- src/components/authentication/LoginVue.vue
+++ src/components/authentication/LoginVue.vue
@@ -2,18 +2,7 @@
   <form @submit.prevent="handleSubmit">
     <div>
       <label class="input input-bordered input-primary flex items-center gap-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          x="0px"
-          y="0px"
-          width="16"
-          height="16"
-          viewBox="0 0 48 48"
-        >
-          <path
-            d="M24 4A10 10 0 1024 24 10 10 0 1024 4zM36.021 28H11.979C9.785 28 8 29.785 8 31.979V33.5c0 3.312 1.885 6.176 5.307 8.063C16.154 43.135 19.952 44 24 44c7.706 0 16-3.286 16-10.5v-1.521C40 29.785 38.215 28 36.021 28z"
-          ></path>
-        </svg>
+        <Icon icon="ic:baseline-person" width="24" height="24" />
         <input
           v-model="form.username"
           type="text"
@@ -25,18 +14,7 @@
     </div>
     
       <label class="input input-bordered input-primary flex items-center gap-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-          fill="currentColor"
-          class="h-4 w-4 opacity-70"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-            clip-rule="evenodd"
-          />
-        </svg>
+        <Icon icon="ic:baseline-key" width="24" height="24" />
         <input
           type="password"
           class="grow"
@@ -65,6 +43,7 @@
 
 <script setup>
 import { reactive, ref } from 'vue';
+import {Icon} from "@iconify/vue";
 import UserService from '../../services/user.service';
 
 const form = reactive({
