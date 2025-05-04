113f87f4c0adf986a2775f4e7fa6d7c99f77823f
Ignacio Sanchez <nachoagusss1@gmail.com>
2025-05-04 16:22:06 -0300

    correcion dockerfile para despliegue

--- vue-shims.d.ts
+++
@@ -1,7 +0,0 @@
-// vue-shims.d.ts
-declare module "*.vue" {
-  import type { DefineComponent } from "vue";
-  // DefineComponent<Props, RawBindings, D> con any para simplificar
-  const component: DefineComponent<{}, {}, any>;
-  export default component;
-}
